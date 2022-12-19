0207f07e707120037fe805dd6c032b526fab0ba6
Gabriel Belo <91219977+gabrielbelo2007@users.noreply.github.com>
2022-12-19 14:50:54 -0300

    Fatorial (Mudanças + Nota do autor)

--- Códigos (Exemplos)/Fatorial - Recursão.py
+++ Códigos (Exemplos)/Fatorial - Recursão.py
@@ -1,13 +1,24 @@
-def fat(x, z, w):
-    z -= 1
-    y = x-1
-    if z == y:
-        w = x * z
-        z -= 1
-    if z <= 1:
+def fat(x, w):
+    if x <= 1:
         print(w)
+        return
+    if w == 0:
+        w = x * (x-1)
+        fat(x-1, w)
     else:
-        w = w * z
-        fat(x, z, w)
+        w = w * (x-1)
+        fat(x-1, w)
 
-fat(7, 7, 0)
+fat(7, 0)
+
+# Nota do autor
+
+"""
+def fat(x):
+    if x == 1:
+        return 1
+    else:
+        return x * fat(x-1)
+
+print(fat(4))
+"""
